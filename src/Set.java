8136c7a9876008ddf3187f373d0e8e5783c14c5e
Victor Timpau <timpauvictor@gmail.com>
2016-01-29 15:19:06 -0500

    syncing to thinkpad

--- src/Set.java
+++ src/Set.java
@@ -1,4 +1,40 @@
+import java.util.Vector;
 
 public class Set {
-
+	Vector<String> mySet = new Vector<String>();
+	
+	public Set(String[] data) {
+		for (String s : data) {
+			this.addElement(s);
+		}
+	}
+	
+	public Set() {
+		//leave empty
+	}
+	
+	public String getElement(int index) {
+		return this.mySet.elementAt(index);
+	}
+	
+	public int getCount() {
+		return this.mySet.size();
+	}
+	
+	public void addElement(String element) {
+		this.mySet.add(element);
+	}
+	
+	public Set setUnion(Set otherSet) {
+		Set newSet = new Set();
+		for (int i = 0; i < otherSet.getCount(); i++) {
+			newSet.addElement(otherSet.getElement(i));
+		}
+		return newSet;
+	}
+	
+	public Set setIntersection(Set otherSet) {
+		Set newSet = new Set();
+		
+	}
 }
