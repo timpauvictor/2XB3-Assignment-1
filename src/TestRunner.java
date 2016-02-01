280def0f1744b3a977a3af33dd0cd6730f36365c
Victor Timpau <timpauvictor@gmail.com>
2016-01-31 21:18:37 -0500

    "finished"

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,3 +1,7 @@
+/*
+ * 
+ */
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -5,10 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestRunner {
+	String[] toWrite = null; //array to store output.txt data
 
 	public static void main(String[] args) {
 		testToString();
 		testUnion();
+		testIntersection();
+		System.out.println("I totally uderestimated the assignment and ran out of time, oops");
 	}
 	
 	public static void testUnion() {
@@ -70,11 +77,54 @@ public class TestRunner {
 	}
 	
 	public static void testIntersection() {
+		System.out.println("Entering testIntersection...");
+		List<String> data = null;
+		try {
+			data = Files.readAllLines(Paths.get("input.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		String[] dataBuilder = data.get(15).split(",");
+		Set set = new Set(dataBuilder);
+		dataBuilder = data.get(16).split(",");
+		Set set2 = new Set(dataBuilder);
+		set = set.setIntersection(set2);
+		if (set.toString().equals(data.get(17))) {
+			System.out.println("Test case 1 passed.");
+		} else {
+			System.out.println("Test case 1 failed.");
+			System.out.println("expected " + data.get(17) + " got " + set.toString());
+		}
+		
+		dataBuilder = data.get(18).split(",");
+		set = new Set(dataBuilder);
+		dataBuilder = data.get(19).split(",");
+		set2 = new Set(dataBuilder);
+		set = set.setIntersection(set2);
+		if (set.toString().equals(data.get(20))) {
+			System.out.println("Test case 2 passed.");
+		} else {
+			System.out.println("Test case 2 failed.");
+			System.out.println("expected " + data.get(20) + " got " + set.toString());
+		}
+		
+		dataBuilder = data.get(21).split(",");
+		set = new Set(dataBuilder);
+		dataBuilder = data.get(22).split(",");
+		set2 = new Set(dataBuilder);
+		set = set.setIntersection(set2);
+		if (set.toString().equals(data.get(23))) {
+			System.out.println("Test case 3 passed.");
+		} else {
+			System.out.println("Test case 3 failed.");
+			System.out.println("expected " + data.get(23) + " got " + set.toString());
+		}
 	}
 	
 	public static void testDifference() {
 		System.out.println("Entering testDifference...");
+		
 	}
 	
 	public static void testProduct() {
