30060ffa192bbc92f98f1ceac693ee7202ba8eae
Victor Timpau <timpauvictor@gmail.com>
2016-01-31 20:16:07 -0500

    finished ADT, started work on testcases

--- src/Set.java
+++ src/Set.java
@@ -10,7 +10,7 @@ public class Set {
 	}
 	
 	public Set() {
-		//leave empty
+		//just make a new empty set
 	}
 	
 	public boolean contains(String element) {
@@ -29,21 +29,77 @@ public class Set {
 		this.mySet.add(element);
 	}
 	
-	public Set setUnion(Set otherSet) {
-		Set newSet = new Set();
-		for (int i = 0; i < otherSet.getCount(); i++) {
-			newSet.addElement(otherSet.getElement(i));
-		}
-		return newSet;
+	public int getCardinality() {
+		Set mySetNoDupes = this.removeDupes();
+		return mySetNoDupes.getCount();
 	}
 	
-	public Set removeDups() {
+	public boolean isEqual(Set otherSet) {
+		String currentElement = "";
+		Set mySetNoDupes = this.removeDupes();
+		Set otherSetNoDupes = otherSet.removeDupes();
+		
+		for (int i = 0; i < mySetNoDupes.getCount(); i++) {
+			currentElement = mySetNoDupes.getElement(i);
+			if (!otherSetNoDupes.contains(currentElement)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public String toString() {
+		String setAsString = "{";
+		for (int i = 0; i < this.getCount(); i++ ) {
+			setAsString += this.getElement(i) + ",";
+		}
+		setAsString += "}";
+		return setAsString;
+	}
+	
+	public boolean isSubset(Set otherSet) {
+		//edge cases
+		if (this.getCount() == 0 && otherSet.getCount() == 0) {
+			return true; //an empty set equals an empty set
+		} else if (this.getCount() == 0 && otherSet.getCount() != 0) {
+			return false;
+		} else if (this.getCount() != 0 && otherSet.getCount() == 0) {
+			return false;
+		}
+		//regular cases
+		String currentElement = "";
+		Set mySetNoDupes = this.removeDupes();
+		Set otherSetNoDupes = otherSet.removeDupes();
+		
+		for (int i = 0; i < mySetNoDupes.getCount(); i++) {
+			currentElement = mySetNoDupes.getElement(i);
+			if (!otherSetNoDupes.contains(currentElement)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public Set setUnion(Set otherSet) {
+		Set newSet = new Set();
+		Set mySetNoDupes = this.removeDupes();
+		Set otherSetNoDupes = otherSet.removeDupes();
+		for (int i = 0; i < otherSetNoDupes.getCount(); i++) { //iterate through set
+			newSet.addElement(otherSetNoDupes.getElement(i)); // add element
+		}
+		for (int i = 0; i < mySetNoDupes.getCount(); i++) {
+			newSet.addElement(otherSetNoDupes.getElement(i)); 
+		}
+		return newSet; //return new set
+	}
+	
+	public Set removeDupes() {
 		Set newSet = new Set();
 		String currentElement = "";
-		for (int i = 0; i < this.getCount(); i++) {
+		for (int i = 0; i < this.getCount(); i++) { //iterate through set
 			currentElement = this.getElement(i);
-			if (!newSet.contains(currentElement)) {
-				newSet.addElement(currentElement);
+			if (!newSet.contains(currentElement)) { //if the newSet doesn't contain our current element
+				newSet.addElement(currentElement); //add it 
 			} else {
 				//do nothing
 			}
@@ -52,8 +108,63 @@ public class Set {
 	}
 	
 	public Set setIntersection(Set otherSet) {
+		//edge cases
+		if (otherSet.getCount() == 0) {
+			return this;
+		} else if (this.getCount() == 0) {
+			return otherSet;
+		}
+		//regular cases
+		String currentElement = "";
 		Set newSet = new Set();
-		Set mySetNoDups = this.removeDups(); 
-		
+		Set mySetNoDupes = this.removeDupes(); 
+		Set otherSetNoDupes = otherSet.removeDupes();
+		for (int i = 0; i < mySetNoDupes.getCount(); i++) { //iterate through set
+			currentElement = mySetNoDupes.getElement(i); //get element 
+			if (otherSetNoDupes.contains(currentElement)) { //check if other set contains it
+				newSet.addElement(currentElement); //add it to newSet if it does
+			}
+		}
+		return newSet;
+	}
+	
+	public Set setDifference(Set otherSet) {
+		//edge cases
+		if (otherSet.getCount() == 0) {
+			return this;
+		} else if (this.getCount() == 0) {
+			return otherSet;
+		}
+		//regular cases
+		String currentElement = "";
+		Set newSet = new Set();
+		Set mySetNoDupes = this.removeDupes(); 
+		Set otherSetNoDupes = otherSet.removeDupes();
+		for (int i = 0; i < mySetNoDupes.getCount(); i++) { //iterate through set
+			currentElement = mySetNoDupes.getElement(i); //get element 
+			if (!otherSetNoDupes.contains(currentElement)) { //check if other set doesn't contain it
+				newSet.addElement(currentElement); //add it to newSet if it does
+			}
+		}
+		return newSet;
+	}
+	
+	public Set setProduct(Set otherSet) {
+		//edge cases
+		if (otherSet.getCount() == 0) {
+			return this;
+		} else if (this.getCount() == 0) {
+			return otherSet;
+		}
+		//regular cases
+		Set newSet = new Set();
+		Set mySetNoDupes = this.removeDupes();
+		Set otherSetNoDupes = otherSet.removeDupes();
+		for (int i = 0; i < mySetNoDupes.getCount(); i++) {
+			for (int j = 0; j < otherSetNoDupes.getCount(); i++) {
+				newSet.addElement("<" + mySetNoDupes.getElement(i) + "," + otherSetNoDupes.getElement(j) + ">");
+			}
+		}
+		return newSet;
 	}
 }
--- src/TestRunner.java
+++ src/TestRunner.java
@@ -2,8 +2,42 @@
 public class TestRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	public static void testUnion() {
+		System.out.println("Entering testUnion...");
+	}
+	
+	public static void testIntersection() {
+		System.out.println("Entering testIntersection...");
+	}
+	
+	public static void testDifference() {
+		System.out.println("Entering testDifference...");
+	}
+	
+	public static void testProduct() {
+		System.out.println("Entering testProduct...");
+	}
+	
+	public static void testIsEqual() {
+		System.out.println("Entering testIsEqual...");
+	}
+	
+	public static void testIsSubset() {
+		System.out.println("Entering testIsSubset...");
+	}
+	
+	public static void testGetCount() {
+		System.out.println("Entering testGetCount...");
+	}
+	
+	public static void testGetCardinality() {
+		System.out.println("Entering testGetCardinality...");
+	}
+	
+	public static void testToString() {
+		System.out.println("Entering testToString..."); 
+	}
 }
